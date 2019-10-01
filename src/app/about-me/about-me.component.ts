266c724cce9ce319facaf46b4a908ee7853d1269
ihab <ihabau@gmail.com>
2019-10-01 02:30:47 +0200

    little ethstatics

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
+
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+    let toggle = false;
+
+    $(".introBtn").click(function () {
+
+
+      if (toggle == false) {
+        $(".intro").removeClass("d-none");
+        $(".intro").show();
+        $(".contact").hide();
+        $(".introBtn").text("contact")
+        $(".introBtn").removeClass("btn-info");
+        $(".introBtn").addClass("btn-danger");
+
+
+        toggle = true;
+
+      } else {
+        $(".intro").hide();
+        $(".contact").show();
+        $(".introBtn").text("Intro")
+        $(".introBtn").removeClass("btn-danger");
+        $(".introBtn").addClass("btn-info");
+
+        toggle = false;
+
+      }
+
+
+
+    })
+
+
   }
 
+
 }
+
+
